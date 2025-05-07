768a671eec5f15ca7d67e4da361300fc791ad83f
hepinamipara <hepinamipara7600@gmail.com>
2025-05-07 16:44:13 +0530

    jenkins uploaded

---
+++ jenkins/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "private" {
+  ami           = "ami-00a929b66ed6e0de6"
+  instance_type = "t2.micro"
+  tags = {
+    Name = myec2jenkins
+  }
+}
